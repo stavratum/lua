1709b79fe9c324e50d9b3cf9d15b27a2c40370c7
stavratum <89344034+stavratum@users.noreply.github.com>
2022-09-21 09:35:32 +0500

    Update other.lua

--- 8531522502/ui/other.lua
+++ 8531522502/ui/other.lua
@@ -1,1 +1,8 @@
+local other = Library.New({ Title = "OTHER" })
 
+other.Button({
+    Text = "Destroy BlurEffect",
+    Callback = function()
+        game:GetService"Lighting":FindFirstChildOfClass"BlurEffect":Destroy()
+    end
+})
