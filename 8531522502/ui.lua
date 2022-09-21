441a8e24b34dd8ba6d6ffbbf121cfe155d6164df
stavratum <89344034+stavratum@users.noreply.github.com>
2022-09-21 09:33:03 +0500

    Update ui.lua

--- 8531522502/ui.lua
+++ 8531522502/ui.lua
@@ -1,1 +1,22 @@
+Library = Material.Load({
+	  Title = GUI_Name,
+    Style = 1,
+    SizeX = 450,
+    SizeY = 350,
+    Theme = "Light",
+    ColorOverrides = { MainFrame = Color3.fromRGB(235, 235, 235) }
+})
 
+-- ui: preparations
+
+PickItem = workspace.PickItem
+
+Weapons = {'Bat', 'Katana', 'FireAxe', 'Machete', 'Crowbar', 'Pipe'}
+Items =  {'Cookie', 'StrangeFruit', 'IceTea', 'FurtaSoda', 'BlueCone', 'Collar', 'blueStim', 'UnstableStim', 'SynthVisor', 'ProtoMask'}
+
+-- ui: main
+
+require("ui/client.lua")()
+require("ui/players.lua")()
+require("ui/weapons.lua")()
+require("ui/other.lua")()
