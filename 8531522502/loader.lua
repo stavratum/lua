e3a44a0635bb7b9badbcd14a8cbb9c431e287d01
stavratum <89344034+stavratum@users.noreply.github.com>
2022-09-21 09:27:37 +0500

    Update loader.lua

--- 8531522502/loader.lua
+++ 8531522502/loader.lua
@@ -1,1 +1,18 @@
+local function require(v)
+    local link = ("https://raw.githubusercontent.com/stavratum/lua/main/8531522502/%s"):format(v)
+    return loadstring( game:HttpGet(link) )
+end
 
+local function main()
+    require("setgenv.lua")()
+    require("ui.lua")()
+  
+    return tick()
+end
+
+print(
+    ("%s :: Loaded in %.2f seconds"):format(
+        script.Name,
+        -( tick() - main() )
+    )
+)
