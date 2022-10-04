b1fab8c647e6b433e02887ddcd21263a65aacc8b
stavratum <89344034+stavratum@users.noreply.github.com>
2022-10-04 08:43:48 +0500

    Update Connections.lua

--- Connections.lua
+++ Connections.lua
@@ -5,16 +5,8 @@ function connections:Open(kv)
     
     local Thread = {}
     
-    function Thread:Add(Object, Signal, OnFire)
-        local index = #self + 1
-        
-        self[index] = Object[Signal]:Connect(OnFire)
-        
-        Object.Destroying:Connect(
-            function()
-                table.remove(self, index)
-            end
-        )
+    function Thread:Add(Signal, OnFire)
+        self[index] = Signal:Connect(OnFire)
     end
     
     function Thread:Close()
@@ -29,17 +21,29 @@ function connections:Open(kv)
         end
     end
     
+    function Thread:Clear()
+        for index, connection in pairs(self) do
+            if connection.Connected then
+                connection:Disconnect()
+            end
+            
+            table.clear(self)
+        end
+    end
+    
     return setmetatable(self.Threads[ kv or (#Threads + 1) ], { __index = __index })
 end
 
 function connections:Destroy()
     for index, thread in next, self.Threads do
         for index, connection in pairs(thread) do 
-            connection:Disconnect()
+            if connection.Connected then
+                connection:Disconnect()
+            end
         end
         
         table.clear(thread)
-        table.remove(self.Threads, index)
+        self.Thread[index] = nil
     end
 end
 
@@ -47,11 +51,25 @@ function connections:Close(kv)
     local thread = self.Threads[kv]
     
     for index, connection in pairs(thread) do
-        connection:Disconnect()
+        if connection.Connected then
+            connection:Disconnect()
+        end
     end
     
     table.clear(thread)
     self.Threads[kv] = nil
+end
+
+function connections:Clear()
+    for index, thread in next, self.Threads do
+        for index, connection in pairs(thread) do
+            if connection.Connected then
+                connecion:Disconnect()
+            end
+        end
+        
+        table.clear(thread)
+    end
 end
 
 return setmetatable({ Threads = {} }, { __index = connections })
