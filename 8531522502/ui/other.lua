697bd487c522294a29ec46a3ed69cf7839133608
stavratum <89344034+stavratum@users.noreply.github.com>
2025-01-21 03:46:32 +0500

    Delete 8531522502 directory

--- 8531522502/ui/other.lua
+++
@@ -1,8 +0,0 @@
-local other = Library.New({ Title = "OTHER" })
-
-other.Button({
-    Text = "Destroy BlurEffect",
-    Callback = function()
-        game:GetService"Lighting":FindFirstChildOfClass"BlurEffect":Destroy()
-    end
-})
